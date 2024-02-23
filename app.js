dbae49e2b3f9af020608a2bb3056c7bc0db7979f
ysoni-dev <ysoni1615@gmail.com>
2024-02-24 01:50:29 +0530

    update

--- app.js
+++ app.js
@@ -1,15 +1,90 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const translate = require('translate-google')
+const langdetect = require('langdetect')
 const cors = require('cors')
+
 const app = express()
 app.use(express.json())
 app.use(bodyparser.json())
 app.use(cors())
+
 const PORT = 4000
 
-app.get('/',(req,res)=>{
-    res.send('helllo world!!!!!!!')
+app.get('/', async function (req, res) {
+    try {
+        await homepage(req, res)
+    } catch (error) {
+        console.log(error)
+    }
 })
+
+const homepage = async (req, res) => {
+    try {
+        res.send('Type /translate in the URL to get results.');
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+// api for translating the text
+
+app.post('/translate', async function (req, res) {
+    try {
+        await textTranslate(req, res)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ error: 'error processing translator.' })
+    }
+})
+
+const textTranslate = async (req, res) => {
+    try {
+        const textToTranslate = req.body.text;
+
+        if (textToTranslate === null || typeof textToTranslate !== 'string' || !textToTranslate.trim()) {
+            return res.status(400).json({ error: 'Text is required in the request body and must be a non-empty string.' });
+        }
+
+        // Check if the content is numeric or not translatable
+        if (/^\d+$/.test(textToTranslate)) {
+            return res.json({
+                message: 'The provided text is invalid and cannot be translated.',
+                originalText: textToTranslate,
+                translatedText: textToTranslate
+            });
+        }
+
+        const detectedLanguage = langdetect.detectOne(textToTranslate);
+
+        if (detectedLanguage === 'fr') {
+            return res.json({
+                message: 'The provided text is already in French.',
+                originalText: textToTranslate,
+                translatedText: textToTranslate
+            });
+        }
+
+        const translation = await translate(textToTranslate, { to: 'fr' });
+
+        if (!translation) {
+            return res.status(500).json({ error: 'Translation failed.' });
+        }
+
+        res.json({ originalText: textToTranslate, translatedText: translation });
+    } catch (error) {
+        console.error(error);
+
+        if (error instanceof SyntaxError) {
+            return res.status(400).json({ error: 'Invalid JSON in the request body.' });
+        }
+
+        res.status(500).json({ error: 'Internal server error.' });
+    }
+}
 
 app.listen(PORT,()=>{
     console.log(`serve is running on ${PORT}`)
